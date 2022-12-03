095a437dde1d23649e378e1464338a86aa194fe9
katherina17 <katherinepetrova76@gmail.com>
2022-12-03 20:02:24 +0300

    feat: add logo and create the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import './App.css';
+import logo from "./assets/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className="App-container">
+         <header>
+             <div className="App-wrapper">
+                     <div className="logo-header">
+                         <img src={logo} alt='logo'/>
+                     </div>
+             </div>
+         </header>
+        <main>
+            <div className="App-wrapper menu-user-container">
+                <nav className="nav-bar">
+                    <ul className="nav-list ">
+                        <li>Profile</li>
+                        <li>Messages</li>
+                        <li>News</li>
+                        <li>Music</li>
+                        <li>Settings</li>
+                    </ul>
+                </nav>
+                <div className="main-container">
+                    <img src="https://cdn1.epicgames.com/ue/product/Screenshot/1-1920x1080-dab564274b400e044d6641ad755ee628.jpg?resize=1&w=1920" alt="landscape"/>
+                    <p>Ava + description</p>
+                    <p>My posts</p>
+                    <p>New post</p>
+                    <p>post1</p>
+                    <p>post2</p>
+                </div>
+            </div>
+        </main>
     </div>
   );
 }
