586efe5af8f4fc21699e083c33dc3513c5736ecd
katherina17 <katherinepetrova76@gmail.com>
2022-12-03 21:34:26 +0300

    fix: changed grid to flex

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,32 @@ function App() {
     <div className="App-container">
          <header>
              <div className="App-wrapper">
-                     <div className="logo-header">
-                         <img src={logo} alt='logo'/>
-                     </div>
+                 <div className="logo-header">
+                     <img src={logo} alt='logo'/>
+                 </div>
              </div>
          </header>
-        <main>
-            <div className="App-wrapper menu-user-container">
-                <nav className="nav-bar">
-                    <ul className="nav-list ">
-                        <li>Profile</li>
-                        <li>Messages</li>
-                        <li>News</li>
-                        <li>Music</li>
-                        <li>Settings</li>
-                    </ul>
-                </nav>
-                <div className="main-container">
-                    <img src="https://cdn1.epicgames.com/ue/product/Screenshot/1-1920x1080-dab564274b400e044d6641ad755ee628.jpg?resize=1&w=1920" alt="landscape"/>
-                    <p>Ava + description</p>
-                    <p>My posts</p>
-                    <p>New post</p>
-                    <p>post1</p>
-                    <p>post2</p>
+            <main>
+                <div className="App-wrapper menu-user-container">
+                    <nav className="nav-bar">
+                        <ul className="nav-list  ">
+                            <li> <a href="#"> Profile </a></li>
+                            <li> <a href="#">Messages </a></li>
+                            <li> <a href="#">News </a></li>
+                            <li> <a href="#">Music </a></li>
+                            <li><a href="#">Settings</a></li>
+                        </ul>
+                    </nav>
+                        <div className="main-container">
+                            <img src="https://cdn1.epicgames.com/ue/product/Screenshot/1-1920x1080-dab564274b400e044d6641ad755ee628.jpg?resize=1&w=1920" alt="landscape"/>
+                            <p>Ava + description</p>
+                            <p>My posts</p>
+                            <p>New post</p>
+                            <p>post1</p>
+                            <p>post2</p>
+                        </div>
                 </div>
-            </div>
-        </main>
+            </main>
     </div>
   );
 }
