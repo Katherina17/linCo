61a3dacc90db3dd7d5f9311011e35616a1591a0c
katherina17 <katherinepetrova76@gmail.com>
2023-02-06 14:37:33 +0300

    feat: change FindPeople to class component

--- src/components/FindPeople/FindPeople.tsx
+++ src/components/FindPeople/FindPeople.tsx
@@ -3,27 +3,30 @@ import {PeopleInfo} from "./PeopleInfo/PeopleInfo";
 import {Button} from "../Button/Button";
 import s from './FindPeople.module.css';
 import axios from "axios";
-
-import {PeopleType} from "../../redux/findPeopleReducer";
 import {v1} from "uuid";
+import React from 'react';
 
-export const FindPeople = (props: FindPeoplePropsType) => {
-    if(props.state.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => props.setUsers(response.data.items));
+
+
+export class FindPeople extends React.Component<FindPeoplePropsType>{
+    constructor(props: FindPeoplePropsType){
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => props.setUsers(response.data.items))
     }
-    return (
-        <section className={s.section}>
-            {props.state.map(el => {
+    render(){
+        return <section className={s.section}>
+            {this.props.state.map(el => {
                 return (
                     <div className={s.infoAndButton} key={v1()}>
                         <PeopleInfo key={el.id}
                                     imgSrc={el.photos.small}
                                     name={el.name}
                                     status={el.status}/>
-                        <Button callBack={ () => el.followed? props.unFollowUser(el.id) : props.followUser(el.id)} key={v1()} name={el.followed? 'Unfollowed' : 'Follow'}/>
+                        <Button callBack={ () => el.followed? this.props.unFollowUser(el.id) : this.props.followUser(el.id)} key={v1()} name={el.followed? 'Unfollowed' : 'Follow'}/>
                     </div>
                 )
             })}
         </section>
-    )
+    }
+
 }
